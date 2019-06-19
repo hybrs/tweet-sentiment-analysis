039e374da2280a44f21b8635f4f7dd62fe9ad283
hybrs <ml.salinas.23@gmail.com>
2019-06-19 11:25:46 +0200

    Update merge_results.py

--- merge_results.py
+++ merge_results.py
@@ -3,22 +3,36 @@ import pickle
 import sys
 import numpy as np
 
-grid_ = [ pickle.load(open('./cv_result/cv_result_'+str(i), 'rb')) for i in range(2) ]
+def print_sorted(scores, score):
+	print("model_tag\t"+score+" +/-std")
+	print("========================================================")
+	for item in sorted(scores[score].items(), key = operator.itemgetter(1), reverse=True):
+		print(item[0]+"\t"+str(item[1][0])[:5]+" +/-"+str(item[1][1])[:5])
+		print("--------------------------------------------------------------------------")
 
-grid_result = dict({})
+
+grid_ = [ pickle.load(open('./cv_result/cv_result_b32-e2-n('+str(i)+', '+str(i)+', '+str(i)+')-k(2, 3, 4)-xTW200', 'rb')) for i in [100, 150, 200, 250, 300, 350] ]
+
+scores_ =  dict({
+	'accuracy' : dict({}),
+	'f1' : dict({}),
+	'mavg_recall' :  dict({})
+	})
 
 for ds in grid_:
 	for model_tag in ds.keys():
-		fold_scores = ds[model_tag] 
-		grid_result.update({
-			model_tag: [np.average(fold_scores),np.std(fold_scores)]
+		scores = ds[model_tag]
+		scores_['accuracy'].update({
+			model_tag: [np.average(scores['accuracy']),np.std(scores['accuracy'])]
+			})
+		scores_['f1'].update({
+			model_tag: [np.average(scores['f1']),np.std(scores['f1'])]
+			})
+		scores_['mavg_recall'].update({
+			model_tag: [np.average(scores['mavg_recall']),np.std(scores['mavg_recall'])]
 			})
 
+for score_ in scores_.keys():
+	print_sorted(scores_, score_)
 
-print("model_tag\tValidation accuracy +/-std")
-print("========================================================")
-for item in sorted(grid_result.items(), key = operator.itemgetter(1), reverse=True):
-	print(item[0]+"\t"+str(item[1][0])[:5]+" +/-"+str(item[1][1])[:5])
-	print("--------------------------------------------------------------------------")
-
-#pickle.dump(file=open('grid_result_'+suffix, 'wb'), obj=grid_result)
+if len(sys.argv) > 1 : pickle.dump(file=open('grid_result_'+sys.argv[1], 'wb'), obj=scores_)
